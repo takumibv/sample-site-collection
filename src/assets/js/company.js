914920753a98124bd8ae1abcf328e0a19774584f
takumibv <everybody.say.hey.yo@gmail.com>
2022-05-04 16:24:27 +0900

    company pc

--- src/assets/js/company.js
+++ src/assets/js/company.js
@@ -1,5 +1,24 @@
 import $ from "jquery";
 
 $(function () {
-  console.log($("body"));
+  const closeAllDrawer = () => $(".drawer--open").removeClass("drawer--open");
+
+  $(document).on("click", ".js-drawer-toggle", (e) => {
+    e.preventDefault();
+
+    const $drawer = $(e.currentTarget).closest(".js-drawer-toggle");
+    const $target = $("#" + $drawer.attr("data-target-id"));
+
+    $target.hasClass("drawer--open")
+      ? $target.removeClass("drawer--open")
+      : $target.addClass("drawer--open");
+  });
+
+  $(window).on("resize", () => {
+    closeAllDrawer();
+  });
+
+  $(window).on("keydown", (e) => {
+    if (e.code === "Escape") closeAllDrawer();
+  });
 });
